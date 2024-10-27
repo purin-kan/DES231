a8bb094fb315d0ddb777b8f5ffb34aaecc0307c2
purin-kan <purinkk9@gmail.com>
2024-10-28 00:03:14 +0700

    lab6 skeleton

---
+++ lab6/Queue.java
@@ -0,0 +1,2 @@
+/*You need your work from Lab5. Copy and Paste here. 
+*/
---
+++ lab6/SNode.java
@@ -0,0 +1,27 @@
+/**
+ * The {@code Node} class represents a node in a singly linked list.
+ *
+ * @param <T> The type of data stored in the node.
+ */
+class SNode<T> {
+	/**
+	 * The data element stored in the node.
+	 */
+	T element;
+
+	/**
+	 * Reference to the next node in the linked list.
+	 */
+	SNode<T> next;
+
+	/**
+	 * Constructs a new node with the specified data element.
+	 *
+	 * @param element The data element to store in the node.
+	 */
+	SNode(T element) {
+		this.element = element;
+		next = null;
+	}
+
+}
